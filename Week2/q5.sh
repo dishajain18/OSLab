36011dd64082716e8c43d95b118a8fb40c17fdca
Disha Jain <dishajain1802@gmail.com>
2024-10-29 21:40:29 +0530

    Update q5.sh

--- Week2/q5.sh
+++ Week2/q5.sh
@@ -1,3 +1,3 @@
-cat $1>sorted.txt
-cat $2>sorted.txt
-sort -u -o sorted.txt sorted.txt
+cat $1>sorted.txt #write file 1 to sorted.txt
+cat $2>>sorted.txt #append file2 from wherever we wrote in sorted.txt last (so that old content not erased)
+sort -u -o sorted.txt sorted.txt  #u is for unique, o is to store output of sort in the second filename (here same file)
